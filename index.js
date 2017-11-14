53ee8371057b7c9413aa033aa05d8fa8815ecf7e
Michael Myung <michael.h.myung@gmail.com>
2017-11-14 01:20:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function printBadges(arrayNames){
+  for(let i =0; i < arrayNames.length; i++){
+    //i //represent the index
+    // arrayNames[i] //represent the element
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+  return arrayNames
+}
+
+function coinToss() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(coinToss()){
+      console.log("Tails!!!")
+      counter++
+    }
+    return(`You got ${counter} tails in a row!`)
+  }
